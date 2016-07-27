7f5941e9b94be94ddccc2f8acf5bff826812eaea
Yukihiko Okuyama <y.okuyama@sumally.com>
2016-07-27 20:18:59 +0900

    webpack-dev-serverの設定をwebpack.config.jsから切り離した

--- server.babel.js
+++ server.babel.js
@@ -6,13 +6,30 @@ import WebpackDevServer from 'webpack-dev-server';
 import webpackConfig from './webpack.config.babel.js';
 
 const port = 3000;
+const url = `http://localhost:${port}`;
 
-new WebpackDevServer(
-    webpack(webpackConfig), webpackConfig.devServer
-).listen(port, 'localhost', (error) => {
+webpackConfig.entry.app.unshift(
+    `webpack-dev-server/client?${url}`,
+    'webpack/hot/only-dev-server'
+);
+
+webpackConfig.plugins.push(
+    new webpack.HotModuleReplacementPlugin()
+);
+
+new WebpackDevServer(webpack(webpackConfig), {
+    publicPath: webpackConfig.output.publicPath,
+    contentBase: 'app/dev',
+    hot: true,
+    historyApiFallback: true,
+    stats: {
+        colors: true,
+        chunks: false,
+    },
+}).listen(port, 'localhost', (error) => {
     if (error) {
         return console.log(error);
     }
 
-    opn(`http://localhost:${port}`, {app: 'google chrome'});
+    opn(url, {app: 'google chrome'});
 });
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,22 +1,25 @@
 'use strict';
 
 import path from 'path';
-import webpack from 'webpack';
 import merge from 'webpack-merge';
 
 import CopyWebpackPlugin from 'copy-webpack-plugin';
 
 const config = {
     common: {
-        context: path.join(__dirname, 'app/dev'),
+        context: path.join(__dirname, 'app', 'dev'),
+        entry: {
+            app: [
+                './assets/js/src/app.jsx',
+            ],
+        },
         module: {
             preLoaders: [
                 {
                     test: /\.js?$/,
                     loaders: [
 
-                        // -loader postfixをつけないとnode_modulesまで対象に含まれてしまうため、
-                        // 省略せずに記述すること
+                        // -loader postfixをつけないとnode_modulesまで対象に含まれてしまう
                         // ref: http://stackoverflow.com/a/29890656
                         'source-map-loader',
                     ],
@@ -28,84 +31,60 @@ const config = {
         },
     },
     dev: {
-        entry: {
-            app: [
-                // HMR用の指定は本番ビルドでは不要なので開発ビルド時のみに含める
-                'webpack-dev-server/client?http://localhost:3000',
-                'webpack/hot/only-dev-server',
-                './assets/js/src/app.jsx',
-            ],
-        },
         output: {
             // 本来は相対パスで問題ないが、
             // server.js経由で参照される場合は絶対パスである必要がある
-            path: path.join(__dirname, 'app/dev/assets/js'),
+            path: path.join(__dirname, 'app', 'dev', 'assets', 'js'),
+            publicPath: '/assets/js/',
             filename: 'app.js',
-        },
-        plugins: [
-            new webpack.HotModuleReplacementPlugin(),
-        ],
-        // webpack-dev-server用の設定
-        devServer: {
-            publicPath: '/',
-            contentBase: 'app/dev',
-            hot: true,
-            historyApiFallback: true,
-            stats: {
-                colors: true,
-                chunks: false,
-            },
         },
         module: {
             loaders: [
                 {
                     test: /\.jsx?$/,
                     loaders: [
-                        // -loader postfixをつけないとnode_modulesまで対象に含まれてしまうため、
-                        // 省略せずに記述すること
+                        // -loader postfixをつけないとnode_modulesまで対象に含まれてしまう
                         // ref: http://stackoverflow.com/a/29890656
                         'react-hot-loader',
                         'babel-loader',
                     ],
-                    include: path.join(__dirname, 'app'),
+                    include: [
+                        path.join(__dirname, 'app'),
+                    ],
                 },
             ],
         },
+        plugins: [],
         devtool: 'inline-source-map',
     },
     prod: {
-        entry: {
-            app: [
-                './assets/js/src/app.jsx',
-            ],
-        },
         output: {
             // 本来は相対パスで問題ないが、
             // server.js経由で参照される場合は絶対パスである必要がある
-            path: path.join(__dirname, 'app/prod/assets/js'),
+            path: path.join(__dirname, 'app', 'prod', 'assets', 'js'),
+            publicPath: '/assets/js/',
             filename: 'app.js',
+        },
+        module: {
+            loaders: [
+                {
+                    test: /\.jsx?$/,
+                    loaders: [
+                        // -loader postfixをつけないとnode_modulesまで対象に含まれてしまう
+                        // ref: http://stackoverflow.com/a/29890656
+                        'babel-loader',
+                    ],
+                },
+            ],
         },
         plugins: [
             new CopyWebpackPlugin([
                 {
                     from: 'index.html',
-                    to: path.join(__dirname, 'app/prod'),
-                }
+                    to: path.join(__dirname, 'app', 'prod'),
+                },
             ]),
         ],
-        module: {
-            loaders: [
-                {
-                    test: /\.jsx?$/,
-                    loaders: [
-                        // -loader postfixをつけないとnode_modulesまで対象に含まれてしまうため、
-                        // 省略せずに記述すること
-                        // ref: http://stackoverflow.com/a/29890656
-                        'babel-loader'
-                    ],
-                },
-            ],
-        },
         devtool: 'eval',
     },
 };
