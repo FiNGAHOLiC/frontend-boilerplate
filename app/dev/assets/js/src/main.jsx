ea3371fef8f276fa28e56adc69c720b693914165
Yukihiko Okuyama <y.okuyama@sumally.com>
2016-07-28 20:19:32 +0900

    コメントが分かりづらかったので修正

--- app/dev/assets/js/src/main.jsx
+++ app/dev/assets/js/src/main.jsx
@@ -13,10 +13,12 @@ ReactDOM.render(
   rootEl
 );
 
+// HMR用の設定
+// ※本番用ビルドでは実行されず、開発環境のみで有効
 if (module.hot) {
   module.hot.accept('./App', () => {
-    // If you use Webpack 2 in ES modules mode, you can
-    // use <App /> here rather than require() a <NextApp />.
+    // HMRさせるために現段階ではrequire()するしかないため
+    // eslintのエラー検知を無視する設定を追加している
     // ref: https://goo.gl/GSa5zv
 
     // eslint-disable-next-line global-require
