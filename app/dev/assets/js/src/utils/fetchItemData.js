e4cd0ad60bf7ceda699c531610b9d5eb841e35b7
Yukihiko Okuyama <y.okuyama@sumally.com>
2016-08-02 00:52:13 +0900

    webpack.optimize.UglifyJsPluginでcompress.warningsをfalseにしているにも関わらずwarningsが出るのでbluebirdでなくbabel-polyfillを使用するようにした

--- app/dev/assets/js/src/utils/fetchItemData.js
+++ app/dev/assets/js/src/utils/fetchItemData.js
@@ -1,31 +1,27 @@
-import Promise from 'bluebird';
-
-const DATA = [
-  {
-    title: 'すごいHTML',
-    author: 'HTML好き夫',
-    desc: 'HTMLのココがすごい！これさえ読めばHTMLのすごい使い方がわかるすごい本です。',
-    price: 800,
-  },
-  {
-    title: 'すごいHTML',
-    author: 'HTML好き夫',
-    desc: 'HTMLのココがすごい！これさえ読めばHTMLのすごい使い方がわかるすごい本です。',
-    price: 800,
-  },
-  {
-    title: 'すごいHTML',
-    author: 'HTML好き夫',
-    desc: 'HTMLのココがすごい！これさえ読めばHTMLのすごい使い方がわかるすごい本です。',
-    price: 800,
-  },
-];
-
 export default function fetchItemData() {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve({
-        data: DATA,
+        data: [
+          {
+            title: 'すごいHTML',
+            author: 'HTML好き夫',
+            desc: 'HTMLのココがすごい！これさえ読めばHTMLのすごい使い方がわかるすごい本です。',
+            price: 800,
+          },
+          {
+            title: 'すごいHTML',
+            author: 'HTML好き夫',
+            desc: 'HTMLのココがすごい！これさえ読めばHTMLのすごい使い方がわかるすごい本です。',
+            price: 800,
+          },
+          {
+            title: 'すごいHTML',
+            author: 'HTML好き夫',
+            desc: 'HTMLのココがすごい！これさえ読めばHTMLのすごい使い方がわかるすごい本です。',
+            price: 800,
+          },
+        ],
       });
     }, 1000);
   });
--- webpack.config.common.babel.js
+++ webpack.config.common.babel.js
@@ -6,6 +6,10 @@ export default {
   context: path.join(__dirname, 'app', 'dev'),
   entry: {
     app: [
+      // PromiseやArray.from、Object.assign等のES6構文を使用可能にする
+      // https://babeljs.io/docs/usage/polyfill/
+      // http://qiita.com/inuscript/items/d2a9d5d4daedaacff924
+      'babel-polyfill',
       './assets/js/src/Main.jsx',
     ],
   },
