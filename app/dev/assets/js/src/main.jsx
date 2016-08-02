6183caebb6dbafd283bfc0a6304eea7bec508528
Yukihiko Okuyama <y.okuyama@sumally.com>
2016-08-02 22:44:03 +0900

    HMRが動作しなかった問題を修正

--- app/dev/assets/js/src/main.jsx
+++ app/dev/assets/js/src/main.jsx
@@ -1,3 +1,5 @@
+import { AppContainer } from 'react-hot-loader';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -7,10 +9,33 @@ import App from './containers/App';
 import configure from './store/index';
 
 const store = configure();
+const elRoot = document.getElementById('js-app');
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('js-app')
+  <AppContainer>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </AppContainer>,
+  elRoot
 );
+
+if (module.hot) {
+  module.hot.accept('./containers/App', () => {
+    // HMRさせるために現段階ではrequire()するしかないため
+    // eslintのエラー検知を無視する設定を追加している
+    // ref: https://goo.gl/GSa5zv
+
+    // eslint-disable-next-line global-require
+    const NextApp = require('./containers/App').default;
+
+    ReactDOM.render(
+      <AppContainer>
+        <Provider store={store}>
+          <NextApp />
+        </Provider>
+      </AppContainer>,
+      elRoot
+    );
+  });
+}
--- app/dev/assets/js/src/store/index.js
+++ app/dev/assets/js/src/store/index.js
@@ -1,16 +1,8 @@
 import { createStore } from 'redux';
 import rootReducer from '../reducers/index';
 
-const configure = (initialState) => {
-  const store = createStore(rootReducer, initialState);
-  if (module.hot) {
-    module.hot.accept('../reducers/index', () => {
-      // eslint-disable-next-line global-require
-      const nextReducer = require('../reducers/index').default;
-      store.replaceReducer(nextReducer);
-    });
-  }
-  return store;
-};
+const configure = (initialState) => (
+  createStore(rootReducer, initialState)
+);
 
 export default configure;
