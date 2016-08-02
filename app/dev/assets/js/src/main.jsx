13e315f967772920f9347441453bac46705dc593
Yukihiko Okuyama <y.okuyama@sumally.com>
2016-08-02 16:39:34 +0900

    Reduxでサンプルを作成

--- app/dev/assets/js/src/main.jsx
+++ app/dev/assets/js/src/main.jsx
@@ -1,44 +1,127 @@
-import { AppContainer } from 'react-hot-loader';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
 
-import App from './App';
+/**
+ * Action
+ */
 
-import fetchItemData from './utils/fetchItemData';
+// Action名の定義
+const SEND = 'SEND';
 
-let itemData = [];
-
-const rootEl = document.getElementById('js-app');
-
-window.addEventListener('load', () => {
-  fetchItemData().then((res) => {
-    itemData = res.data;
-    ReactDOM.render(
-      <AppContainer>
-        <App items={res.data} />
-      </AppContainer>,
-      rootEl
-    );
-  });
-}, false);
-
-// HMR用の設定
-// ※本番用ビルドでは実行されず、開発環境のみで有効
-if (module.hot) {
-  module.hot.accept('./App', () => {
-    // HMRさせるために現段階ではrequire()するしかないため
-    // eslintのエラー検知を無視する設定を追加している
-    // ref: https://goo.gl/GSa5zv
-
-    // eslint-disable-next-line global-require
-    const NextApp = require('./App').default;
-
-    ReactDOM.render(
-      <AppContainer>
-        <NextApp items={itemData} />
-      </AppContainer>,
-      rootEl
-    );
-  });
+// Action Creators
+function send(value) {
+  // Action
+  return {
+    type: SEND,
+    value,
+  };
 }
+
+/**
+ * Reducer
+ */
+
+function formReducer(state, action) {
+  switch (action.type) {
+    case 'SEND':
+      return Object.assign({}, state, {
+        value: action.value,
+      });
+    default:
+      return state;
+  }
+}
+
+/**
+ * Store
+ */
+
+// 初期stateの作成
+const initialState = {
+  value: null,
+};
+
+// createStoreメソッドを使ってStoreの作成
+const store = createStore(formReducer, initialState);
+
+/**
+ * View
+ */
+
+// Container Components
+const FormApp = (props) => (
+  <div>
+    <FormInput handleClick={props.onClick} />
+    <FormDisplay data={props.value} />
+  </div>
+);
+
+FormApp.propTypes = {
+  onClick: React.PropTypes.func.isRequired,
+  value: React.PropTypes.string,
+};
+
+// View (Presentational Components)
+class FormInput extends React.Component {
+  send(event) {
+    event.preventDefault();
+    this.props.handleClick(this.myInput.value.trim());
+    this.myInput.value = '';
+  }
+  render() {
+    return (
+      <form>
+        <input type="text" ref={(ref) => (this.myInput = ref)} defaultValue="" />
+        <button onClick={(event) => this.send(event)}>Send</button>
+      </form>
+    );
+  }
+}
+
+FormInput.propTypes = {
+  handleClick: React.PropTypes.func.isRequired,
+};
+
+// View (Presentational Components)
+const FormDisplay = (props) => (
+  <div>{props.data}</div>
+);
+
+FormDisplay.propTypes = {
+  data: React.PropTypes.string,
+};
+
+/**
+ * Connect to Redux
+ */
+function mapStateToProps(state) {
+  return {
+    value: state.value,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onClick(value) {
+      dispatch(send(value));
+    },
+  };
+}
+
+const AppContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(FormApp);
+
+/**
+ * Rendering
+ */
+
+ReactDOM.render(
+  <Provider store={store}>
+    <AppContainer />
+  </Provider>,
+  document.getElementById('js-app')
+);
